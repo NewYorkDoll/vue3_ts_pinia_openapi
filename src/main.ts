cc690597690d9129ac3e1865e026d59b3792db8f
NewYorkDoll <yiziluoying@gmail.com>
2022-11-01 11:46:32 +0800

    add setupHttp

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import pinia from './store'
+import { setupHttp } from './lib/http'
+
+setupHttp()
 
 createApp(App).use(pinia).mount('#app')
